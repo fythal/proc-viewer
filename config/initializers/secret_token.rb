4e5d07b5f74f547a6ab9f1a10af73b05d084e7c7
Kazukiyo UEDA <kazukiyo.ueda@gmail.com>
2013-09-10 00:15:32 +0900

    最初のコミット

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ProcViewer::Application.config.secret_key_base = 'c637008c9da3f690e5325aa9523268cf8ef26a9cf950ffe15cb4486270f4b2c5cf06dfcb4b2363a620b99c4c6297e854d77a15ae5346c2b56d1142f6eecce515'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ProcViewer::Application.config.session_store :cookie_store, key: '_proc-viewer_session'
