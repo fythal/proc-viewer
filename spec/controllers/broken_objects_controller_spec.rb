f23b7f94e99a97e51bbccae9f752954c0aeca0c3
Kazukiyo UEDA <kazukiyo.ueda@gmail.com>
2013-09-10 13:53:22 +0900

    BrokenObjectController は壊れた警報をセットするというスペックを規定

--- spec/controllers/broken_objects_controller_spec.rb
+++ spec/controllers/broken_objects_controller_spec.rb
@@ -1,5 +1,22 @@
+# -*- coding: utf-8 -*-
 require 'spec_helper'
 
 describe BrokenObjectsController do
 
+  def valid_session
+    {}
+  end
+
+  it "Ann クラスに broken を送る" do
+    Ann.should_receive(:broken)
+    get :index, {}, valid_session
+  end
+
+  it "@broken_anns に Ann.broken で返される値をセットする" do
+    ann = Object.new
+    Ann.stub(:broken).and_return([ann])
+
+    get :index, {}, valid_session
+    expect(assigns(:broken_anns)).to eq([ann])
+  end
 end
