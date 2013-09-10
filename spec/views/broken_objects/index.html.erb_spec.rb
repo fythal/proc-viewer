38f544c2776d65be5976dcbeab2433bc342dd4b1
Kazukiyo UEDA <kazukiyo.ueda@gmail.com>
2013-09-10 13:00:25 +0900

    壊れたものとして警報を割り当ててリストアップされるスペックを規定

--- spec/views/broken_objects/index.html.erb_spec.rb
+++ spec/views/broken_objects/index.html.erb_spec.rb
@@ -2,9 +2,16 @@
 require 'spec_helper'
 
 describe "broken_objects/index" do
+  def anns_without_proc
+    [
+      stub_model(Ann, :name => "Ann without proc")
+    ]
+  end
+
   it "broken_anns という id の要素がある" do
+    assign(:broken_anns, anns_without_proc)
     render
-    assert_select "#broken_anns"
+    assert_select "#broken_anns a", :text => "Ann without proc"
   end
 
 end
