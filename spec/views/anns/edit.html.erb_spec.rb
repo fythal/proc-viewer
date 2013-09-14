8873ccecb0d9675317e4d75ef2ad9ba909b9795f
Kazukiyo UEDA <kazukiyo.ueda@gmail.com>
2013-09-15 06:57:05 +0900

    最新のフォームのためのアサーションを実行

--- spec/views/anns/edit.html.erb_spec.rb
+++ spec/views/anns/edit.html.erb_spec.rb
@@ -5,7 +5,6 @@ describe "anns/edit" do
   before(:each) do
     @ann = assign(:ann, stub_model(Ann,
       :name => "MyString",
-      :pdf => "MyString"
     ))
   end
 
@@ -15,11 +14,18 @@ describe "anns/edit" do
     # Run the generator again with the --webrat flag if you want to use webrat matchers
     assert_select "form", :action => anns_path(@ann), :method => "post" do
       assert_select "input#ann_name", :name => "ann[name]"
-      assert_select "input#ann_pdf", :name => "ann[pdf]"
-      assert_select "label[for='ann_panel']", I18n.t(:panel_number)
-      assert_select "input#ann_panel", :name => "ann[panel]"
-      assert_select "label[for='ann_window']", I18n.t(:window_number)
-      assert_select "input#ann_window", :name => "ann[window]"
+
+      # 警報パネル番号
+      assert_select "label[for='ann_panel_number']", I18n.t("helpers.label.ann.panel_number")
+      assert_select "input#ann_panel_number", :name => "ann[panel_number]"
+
+      # 警報窓番号
+      assert_select "label[for='ann_panel_location']", I18n.t("helpers.label.ann.panel_location")
+      assert_select "input#ann_panel_location", :name => "ann[panel_location]"
+
+      # 手順書
+      assert_select "label[for='ann_procedure']", I18n.t("helpers.label.ann.procedure")
+      assert_select "input[type='file']#ann_procedure", :name => "ann[procedure]"
     end
   end
 end
