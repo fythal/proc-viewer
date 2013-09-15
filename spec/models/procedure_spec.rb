abb5afd4094b8e16b2e9008c13d679a1bcb9b6f3
Kazukiyo UEDA <kazukiyo.ueda@gmail.com>
2013-09-15 17:07:12 +0900

    ファイルシステムでのパスを返す #file_path のスペックを規定

--- spec/models/procedure_spec.rb
+++ spec/models/procedure_spec.rb
@@ -27,4 +27,20 @@ describe Procedure do
       end
     end
   end
+
+  describe "#file_path" do
+    context "path 属性が設定されている場合" do
+      it "Rails の public ディレクトリ + path を返す" do
+        procedure = Procedure.new(path: "/foo/bar")
+        expect(procedure.file_path).to eq(Rails.public_path.join(procedure.path.sub(%r|^/|, "")).to_s)
+      end
+    end
+
+    context "path 属性が設定されていない場合" do
+      it "nil を返す" do
+        procedure = Procedure.new
+        expect(procedure.file_path).to be_nil
+      end
+    end
+  end
 end
