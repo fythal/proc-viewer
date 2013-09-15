021a0fd40f571ebc93d8c4fd97d593ff35584179
Kazukiyo UEDA <kazukiyo.ueda@gmail.com>
2013-09-15 22:42:58 +0900

    Location#to_s は location 属性の値を返すように実装

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,4 +1,8 @@
 class Location < ActiveRecord::Base
   belongs_to :panel
   belongs_to :ann
+
+  def to_s
+    location
+  end
 end
--- spec/models/location_spec.rb
+++ spec/models/location_spec.rb
@@ -1,4 +1,11 @@
+# -*- coding: utf-8 -*-
 require 'spec_helper'
 
 describe Location do
+  describe "#to_s" do
+    it "設定された場所 (location) を返す" do
+      loc = Location.new(location: "a1")
+      expect(loc.to_s).to eq("a1")
+    end
+  end
 end
