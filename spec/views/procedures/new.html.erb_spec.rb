4921d040e69e6c060e4cb39c5d89fffc1daa0ef9
Kazukiyo UEDA <kazukiyo.ueda@gmail.com>
2013-09-25 17:57:16 +0900

    手順書の新規作成画面で、警報に割り当てられている最新の手順書の表示についてのスペックを追加

--- spec/views/procedures/new.html.erb_spec.rb
+++ spec/views/procedures/new.html.erb_spec.rb
@@ -21,7 +21,6 @@ describe "procedures/new" do
       assert_select "input#procedure_ann_id[name=?]", "procedure[ann_id]"
       assert_select "input#procedure_revision[name=?]", "procedure[revision]"
       assert_select "input#procedure_revised_on[name=?]", "procedure[revised_on]"
-      assert_select "input#procedure_prev_revision_id[name=?]", "procedure[prev_revision_id]"
     end
   end
 
@@ -30,4 +29,33 @@ describe "procedures/new" do
     render
     assert_select "#ann_name", /MyAnnName/
   end
+
+
+  describe "ある警報に割り当てられている最新の手順書" do
+    describe "警報には手順書が割り当てられていない" do
+      before(:each) do
+        assign(:ann, stub_model(Ann, :name => "MyAnnName", :procedures => []))
+      end
+
+      it "最新の手順書の改定番号の欄には「(手順書なし)」と表示される" do
+        render
+        assert_select "#prev_revision", text: /\(手順書なし\)/
+      end
+    end
+
+    describe "警報には手順書が割り当てられている" do
+      before(:each) do
+        assign(:ann, stub_model(Ann, :name => "MyAnnName",
+                                :procedure => stub_model(Procedure, revision: 1, id: 101),
+                                :procedures => [stub_model(Procedure, revision: 0, id: 100), stub_model(Procedure, revision: 1, id: 101),]))
+      end
+
+      it "最新の手順書の改定番号が表示される" do
+        render
+        assert_select "#prev_revision", text: /1/
+      end
+    end
+  end
+
 end
+
