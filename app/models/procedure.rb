fcc2590e8b13fe0b15e5816ebd9103f14814fd04
Kazukiyo UEDA <kazukiyo.ueda@gmail.com>
2013-09-25 21:24:31 +0900

    手順書のリビジョンを辿るメソッドの廃止

--- app/models/procedure.rb
+++ app/models/procedure.rb
@@ -1,7 +1,5 @@
 class Procedure < ActiveRecord::Base
   belongs_to :ann
-  belongs_to :prev_revision, :class_name => Procedure
-  has_one :next_revision, :class_name => Procedure, :foreign_key => "prev_revision_id"
 
   def latest_revision
     proc = self
