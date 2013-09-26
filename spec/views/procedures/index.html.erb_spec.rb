de277911b835ecc7452648f4aa8014c4eb24a01b
Kazukiyo UEDA <kazukiyo.ueda@gmail.com>
2013-09-26 22:57:06 +0900

    手順書の一覧および詳細ページでの要素に id を追加

--- spec/views/procedures/index.html.erb_spec.rb
+++ spec/views/procedures/index.html.erb_spec.rb
@@ -3,27 +3,17 @@ require 'spec_helper'
 describe "procedures/index" do
   before(:each) do
     assign(:procedures, [
-      stub_model(Procedure,
-        :path => "Path",
-        :ann_id => 1,
-        :revision => "9",
-        :revised_on => "Revised On",
-      ),
-      stub_model(Procedure,
-        :path => "Path",
-        :ann_id => 1,
-        :revision => "9",
-        :revised_on => "Revised On",
-      )
-    ])
+      stub_model(Procedure, :path => "/foo/bar.pdf", :ann_id => 1, :revision => "4", :revised_on => "2013/9/26"),
+      stub_model(Procedure, :path => "/foo/bar.pdf", :ann_id => 1, :revision => "4", :revised_on => "2013/9/26")
+           ])
   end
 
   it "renders a list of procedures" do
     render
-    # Run the generator again with the --webrat flag if you want to use webrat matchers
-    assert_select "tr>td", :text => "Path".to_s, :count => 2
+
+    assert_select "tr>td", :text => "bar.pdf".to_s, :count => 2
     assert_select "tr>td", :text => 1.to_s, :count => 2
-    assert_select "tr>td", :text => "9", :count => 2
-    assert_select "tr>td", :text => "Revised On".to_s, :count => 2
+    assert_select "tr>td", :text => /4/, :count => 2
+    assert_select "tr>td", :text => /2013-09-26/, :count => 2
   end
 end
--- spec/views/procedures/show.html.erb_spec.rb
+++ spec/views/procedures/show.html.erb_spec.rb
@@ -2,20 +2,17 @@ require 'spec_helper'
 
 describe "procedures/show" do
   before(:each) do
-    @procedure = assign(:procedure, stub_model(Procedure,
-      :path => "Path",
-      :ann_id => 1,
-      :revision => "",
-      :revised_on => "Revised On",
-    ))
+    @procedure = assign(:procedure, stub_model(Procedure, :path => "/foo/bar.pdf", :ann_id => 1, :revision => 0, :revised_on => "2013/1/1"))
+    @ann = assign(:ann, stub_model(Ann, :name => "foo"))
+    Ann.stub(:find).with(1).and_return(@ann)
   end
 
   it "renders attributes in <p>" do
     render
-    # Run the generator again with the --webrat flag if you want to use webrat matchers
-    rendered.should match(/Path/)
-    rendered.should match(/1/)
-    rendered.should match(//)
-    rendered.should match(/Revised On/)
+
+    assert_select('#path', text: %r|/foo/bar.pdf|)
+    assert_select('#ann_name', text: /foo/)
+    assert_select('#revision', text: /0/)
+    assert_select('#revised_on', text: /2013-01-01/)
   end
 end
