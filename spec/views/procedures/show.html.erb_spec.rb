56dc24af2233a34d5e1ee3be72bf0c176188dbe3
Kazukiyo UEDA <kazukiyo.ueda@gmail.com>
2013-09-27 07:01:56 +0900

    procedure#show のスペックと実装

--- spec/views/procedures/show.html.erb_spec.rb
+++ spec/views/procedures/show.html.erb_spec.rb
@@ -1,18 +1,44 @@
+# -*- coding: utf-8 -*-
 require 'spec_helper'
 
 describe "procedures/show" do
+
+  def valid_attributes
+    {
+      :path => "/foo/bar.pdf",
+      :revision => "9",
+      :revised_on => "2013/1/1",
+    }
+  end
+
+  def valid_ann_attributes
+    {
+      :id => 1,
+      :name => "foo-bar",
+    }
+  end
+
+  def assign_ann
+    Procedure.any_instance.stub(:ann).and_return(stub_model(Ann, valid_ann_attributes))
+  end
+
   before(:each) do
-    @procedure = assign(:procedure, stub_model(Procedure, :path => "/foo/bar.pdf", :ann_id => 1, :revision => 0, :revised_on => "2013/1/1"))
-    @ann = assign(:ann, stub_model(Ann, :name => "foo"))
-    Ann.stub(:find).with(1).and_return(@ann)
+    @procedure = assign(:procedure, stub_model(Procedure, valid_attributes))
+    @ann = assign(:ann, stub_model(Ann, valid_ann_attributes))
   end
 
   it "renders attributes in <p>" do
     render
 
-    assert_select('#path', text: %r|/foo/bar.pdf|)
-    assert_select('#ann_name', text: /foo/)
-    assert_select('#revision', text: /0/)
+    assert_select('#ann_name', text: /foo-bar/)
+    assert_select('#revision', text: /9/)
     assert_select('#revised_on', text: /2013-01-01/)
+    assert_select('#path', text: %r|/foo/bar.pdf|)
+  end
+
+  it "警報の編集画面へのリンクを表示する" do
+    render
+
+    assert_select('a[href=?]', edit_ann_path(@ann), text: I18n.t(:editing_ann) )
   end
 end
