babc47e6ab5199cb03d74007ce9d68b07d0a7512
Kazukiyo UEDA <kazukiyo.ueda@gmail.com>
2013-09-26 14:15:21 +0900

    警報のサブリソースとして手順書を新規作成するときのフォーム

--- spec/views/procedures/new.html.erb_spec.rb
+++ spec/views/procedures/new.html.erb_spec.rb
@@ -3,59 +3,65 @@ require 'spec_helper'
 
 describe "procedures/new" do
   before(:each) do
-    assign(:procedure, stub_model(Procedure,
-      :path => "MyString",
-      :ann_id => 1,
-      :revision => "",
-      :revised_on => "MyString",
-    ).as_new_record)
+    @procedure = stub_model(Procedure, :path => "MyString", :ann_id => 1, :revision => "", :revised_on => "MyString").as_new_record
+    assign(:procedure, @procedure)
   end
 
-  it "renders new procedure form" do
-    render
-
-    # Run the generator again with the --webrat flag if you want to use webrat matchers
-    assert_select "form[action=?][method=?]", procedures_path, "post" do
-      assert_select "input#procedure_path[name=?]", "procedure[path]"
-      assert_select "input#procedure_ann_id[name=?]", "procedure[ann_id]"
-      assert_select "input#procedure_revision[name=?]", "procedure[revision]"
-      assert_select "input#procedure_revised_on[name=?]", "procedure[revised_on]"
-      assert_select "input#procedure_file[name=?][type=?]", "procedure[file]", "file"
+  describe "新規の手順書が警報に割り当てられる場合" do
+    before(:each) do
+      @ann = stub_model(Ann, id: 1, name: "MyAnnName")
+      assign(:ann, @ann)
     end
-  end
 
-  it "警報 @ann が代入されていれば、警報名称を表示する" do
-    assign(:ann, stub_model(Ann, :name => "MyAnnName"))
-    render
-    assert_select "#ann_name", /MyAnnName/
-  end
+    it "アクションに警報の id が含まれたフォームを描画する" do
+      render
 
-
-  describe "ある警報に割り当てられている最新の手順書" do
-    describe "警報には手順書が割り当てられていない" do
-      before(:each) do
-        assign(:ann, stub_model(Ann, :name => "MyAnnName", :procedures => []))
-      end
-
-      it "最新の手順書の改定番号の欄には「(手順書なし)」と表示される" do
-        render
-        assert_select "#prev_revision", text: /\(手順書なし\)/
+      assert_select "form[action=?][method=?]", ann_procedures_path(@ann), "post" do
+        # assert_select "input#procedure_path[name=?]", "procedure[path]"
+        # assert_select "input#procedure_ann_id[name=?]", "procedure[ann_id]"
+        assert_select "input#procedure_revision[name=?]", "procedure[revision]"
+        assert_select "input#procedure_revised_on[name=?]", "procedure[revised_on]"
+        assert_select "input#procedure_file[name=?][type=?]", "procedure[file]", "file"
       end
     end
 
-    describe "警報には手順書が割り当てられている" do
-      before(:each) do
-        assign(:ann, stub_model(Ann, :name => "MyAnnName",
-                                :procedure => stub_model(Procedure, revision: 1, id: 101),
-                                :procedures => [stub_model(Procedure, revision: 0, id: 100), stub_model(Procedure, revision: 1, id: 101),]))
+    it "警報名称を表示する" do
+      render
+      assert_select "#ann_name", /#{@ann.name}/
+    end
+
+    describe "警報に割り当てられている手順書の最新の情報の表示" do
+      describe "警報には手順書が割り当てられている" do
+        it "最新の手順書の改定番号が表示される" do
+          assign(:ann, stub_model(Ann, :name => "MyAnnName",
+                                  :procedure => stub_model(Procedure, revision: 1, id: 101),
+                                  :procedures => [stub_model(Procedure, revision: 0, id: 100), stub_model(Procedure, revision: 1, id: 101),]))
+          render
+          assert_select "#prev_revision", text: /1/
+        end
       end
 
-      it "最新の手順書の改定番号が表示される" do
-        render
-        assert_select "#prev_revision", text: /1/
+      describe "警報には手順書が割り当てられていない" do
+        it "最新の手順書の改定番号の欄には「(手順書なし)」と表示される" do
+          assign(:ann, stub_model(Ann, :name => "MyAnnName", :procedure => nil, :procedures => []))
+          render
+          assert_select "#prev_revision", text: /\(手順書なし\)/
+        end
       end
     end
   end
 
+  describe "新規の手順書がどの警報にも割り当てられない場合" do
+    it "renders new procedure form" do
+      render
+
+      assert_select "form[action=?][method=?]", procedures_path, "post" do
+        assert_select "input#procedure_path[name=?]", "procedure[path]"
+        assert_select "input#procedure_ann_id[name=?]", "procedure[ann_id]"
+        assert_select "input#procedure_revision[name=?]", "procedure[revision]"
+        assert_select "input#procedure_revised_on[name=?]", "procedure[revised_on]"
+        assert_select "input#procedure_file[name=?][type=?]", "procedure[file]", "file"
+      end
+    end
+  end
 end
-
