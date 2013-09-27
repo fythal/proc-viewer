4194835922ae41e9f67bc6b47eb8e0f27756b394
Kazukiyo UEDA <kazukiyo.ueda@gmail.com>
2013-09-27 16:26:16 +0900

    Procedure#write を実装

--- app/models/procedure.rb
+++ app/models/procedure.rb
@@ -12,4 +12,20 @@ class Procedure < ActiveRecord::Base
   def file_path
     path and Rails.public_path.join(path.sub(%r|^/|, "")).to_s
   end
+
+  def system_path
+    self.path.nil? ? nil : "#{Rails.public_path}#{self.path}"
+  end
+
+  def filename
+    self.path.nil? ? nil : self.path.sub(%r|.*/|, "")
+  end
+
+  def write(uploaded_file)
+    pathname = Pathname.new("/procs")
+    self.path = pathname.join("ann-#{Time.now.strftime('%Y-%m-%d-%H_%M_%S')}.pdf").to_s
+    File.open(self.system_path, 'wb') do |file|
+      file.write(uploaded_file.read)
+    end
+  end
 end
