fe1cacb9a6bdaacc716f55a450f8e979ca381ac9
Kazukiyo UEDA <kazukiyo.ueda@gmail.com>
2013-09-29 05:39:18 +0900

    ユーザーモデル User を追加したことによるスキーマ変更

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130928074028) do
+ActiveRecord::Schema.define(version: 20130928134829) do
 
   create_table "anns", force: true do |t|
     t.string   "name"
@@ -46,6 +46,12 @@ ActiveRecord::Schema.define(version: 20130928074028) do
     t.string   "keywords"
     t.string   "kind"
     t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", force: true do |t|
+    t.string   "name"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
