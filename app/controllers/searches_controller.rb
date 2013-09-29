32cdcef61e15815b3ba3bf42f2ad7c6c074184b4
Kazukiyo UEDA <kazukiyo.ueda@gmail.com>
2013-09-30 01:26:37 +0900

    検索を処理するコントローラの実装

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,4 +1,34 @@
+# -*- coding: utf-8 -*-
 class SearchesController < ApplicationController
+  before_action :set_search, only: [:show]
+
+  # GET /searches/1
+  # GET /searches/1.json
+  def show
+    @anns = Ann.search(@search.keywords)
+  end
+
+  # POST /searches
+  # POST /searches.json
   def create
+    @search = Search.new(search_params)
+
+    respond_to do |format|
+      if @search.save
+        format.html { redirect_to @search }
+      else
+        format.html { redirect_to :back }
+      end
+    end
+  end
+
+  private
+
+  def search_params
+    params.require(:search).permit(:keywords)
+  end
+
+  def set_search
+    @search = Search.find(params[:id])
   end
 end
