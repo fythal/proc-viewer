213050cad285caab49ee3f060436f136769e8d96
Kazukiyo UEDA <kazukiyo.ueda@gmail.com>
2013-09-29 13:59:03 +0900

    ユーザーがログインをしたときの挙動のスペックを規定

--- spec/controllers/logins_controller_spec.rb
+++ spec/controllers/logins_controller_spec.rb
@@ -16,4 +16,24 @@ describe LoginsController do
       expect(response).to render_template("new")
     end
   end
+
+  describe "POST create" do
+    def attributes
+      { "user_id" => "1", "search" => { "keywords" => "foo" } }
+    end
+    before(:each) do
+      User.stub(:find).with("1").and_return(stub_model(Ann, id: 1, name: "foo"))
+      Ann.stub(:search).and_return([stub_model(Ann, name: "foo-bar")])
+    end
+    it "Login オブジェクトを生成する" do
+      expect { post :create, attributes, {} }.to change(Login, :count).by(1)
+    end
+    it "session に Login オブジェクトを格納する" do
+      post :create, attributes, {}
+      expect(session[:login]).to be_kind_of(Login)
+    end
+    it "検索の途中であれば検索結果の画面にリダイレクトする" do
+      response.should redirect_to(search_url(1))
+    end
+  end
 end
