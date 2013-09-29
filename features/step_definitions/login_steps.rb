1e72b3bb863d91984d2fd84546213711c1a8e0ca
Kazukiyo UEDA <kazukiyo.ueda@gmail.com>
2013-09-29 12:50:41 +0900

    ユーザー登録があらかじめやっておく必要あり

--- features/step_definitions/login_steps.rb
+++ features/step_definitions/login_steps.rb
@@ -3,6 +3,10 @@
 Given(/^そのパソコンにはユーザーの記録が残っていない$/) do
 end
 
+Given(/^自分は "(.*?)" という名前であらかじめユーザー登録してある$/) do |name|
+  User.create!(name: name)
+end
+
 When(/^ログイン画面が表示されるので、そこで自分の名前 "(.*?)" を選択する$/) do |name|
   select name
 end
