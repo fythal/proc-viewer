5eccd12f50661f0160dd47419fe3dafe758e9e47
Kazukiyo UEDA <kazukiyo.ueda@gmail.com>
2013-10-04 06:35:00 +0900

    Procedure#show のビューのスペックと実装 (仮)

--- spec/views/panels/show.html.erb_spec.rb
+++ spec/views/panels/show.html.erb_spec.rb
@@ -1,15 +1,42 @@
+# -*- coding: utf-8 -*-
 require 'spec_helper'
 
 describe "panels/show" do
   before(:each) do
     @panel = assign(:panel, stub_model(Panel,
-      :number => "Number"
-    ))
+                                       :number => "Number",
+                                       :height => 3,
+                                       :width  => 3,
+                                       ))
+    procedure = stub_model(Procedure)
+    procedure.stub(:path).and_return("/foo/bar.pdf")
+    ann = stub_model(Ann, name: "foo", procedures: [procedure])
+
+    @panel.assign(ann, to: "a1")
   end
 
-  it "renders attributes in <p>" do
+  it "警報パネルの名称を表示する" do
     render
-    # Run the generator again with the --webrat flag if you want to use webrat matchers
     rendered.should match(/Number/)
+  end
+
+  it "テーブルを描画する" do
+    render
+    assert_select "table"
+  end
+
+  it "テーブルに 3 つの行数を描画する" do
+    render
+    assert_select "table tr", 3
+  end
+
+  it "テーブルに 9 つの td 要素を描画する" do
+    render
+    assert_select "table tr td", 9
+  end
+
+  it "テーブルの td 要素には、場所情報を含む id を設定する" do
+    render
+    assert_select "table tr td#loc_a1"
   end
 end
