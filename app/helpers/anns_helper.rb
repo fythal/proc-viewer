bdbc51ab2c841b0657bde309435a939b15aa7c9b
Kazukiyo UEDA <kazukiyo.ueda@gmail.com>
2013-10-05 22:21:10 +0900

    警報の手順書へのリンク、手順書がなかったら名称のみを表示するヘルパーのスペックの規定および実装

--- app/helpers/anns_helper.rb
+++ app/helpers/anns_helper.rb
@@ -1,2 +1,13 @@
 module AnnsHelper
+
+  def link_or_name(ann)
+    return "" if ann.nil?
+
+    if ann.procedure.nil?
+      ann.name
+    else
+      link_to ann.name, ann.procedure.path
+    end
+  end
+
 end
--- spec/helpers/anns_helper_spec.rb
+++ spec/helpers/anns_helper_spec.rb
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 require 'spec_helper'
 
 # Specs in this file have access to a helper object that includes
@@ -11,4 +12,29 @@ require 'spec_helper'
 #   end
 # end
 describe AnnsHelper do
+  describe "link_or_name" do
+    describe "引数が警報オブジェクトで手順書が設定されている" do
+      it "手順書へのリンクを返す" do
+        procedure = stub_model(Procedure, path: "/foo/bar.pdf", filename: "bar.pdf")
+        ann = stub_model(Ann, name: "foo", procedure: procedure)
+        expect(helper.link_or_name(ann)).to match /^<a /
+      end
+      it "手順書へ名称を含む" do
+        procedure = stub_model(Procedure, path: "/foo/bar.pdf", filename: "bar.pdf")
+        ann = stub_model(Ann, name: "bazz", procedure: procedure)
+        expect(helper.link_or_name(ann)).to match /bazz/
+      end
+    end
+    describe "引数が警報オブジェクトで手順書が設定されていない" do
+      it "警報の名称を返す" do
+        ann = stub_model(Ann, name: "bazz")
+        expect(helper.link_or_name(ann)).to eq("bazz")
+      end
+    end
+    describe "引数が nil のとき" do
+      it "空白を返す" do
+        expect(helper.link_or_name(nil)).to eq("")
+      end
+    end
+  end
 end
