e327381a46ecb8a7de43b851f6ddb5caa390bb00
Kazukiyo UEDA <kazukiyo.ueda@gmail.com>
2013-10-08 06:51:08 +0900

    警報パネル一覧を表示する機能のステップを定義

--- features/step_definitions/panel_steps.rb
+++ features/step_definitions/panel_steps.rb
@@ -24,6 +24,17 @@ Given(/^そのパネルの (.*) に警報が割り当てられている$/) do |l
   @panel.assign(ann, to: location)
 end
 
+Given(/^盤 (.*) がある$/) do |name|
+  Board.create!(name: name)
+end
+
+Given(/^盤 (.*) には (.*) と (.*) の警報パネルが配置されている$/) do |board_name, panel_name_one, panel_name_two|
+  board = Board.find_by name: board_name
+  board.panels << Panel.new(number: panel_name_one)
+  board.panels << Panel.new(number: panel_name_two)
+  expect(board.panels.count).to eq(2)
+end
+
 When(/^その表の詳細画面を表示する$/) do
   visit panel_path(@panel)
 end
@@ -38,6 +49,10 @@ end
 
 When(/^編集の実行のボタンをクリックする$/) do
   click_button 'Update Panel'
+end
+
+When(/^警報パネル一覧のページを描画する$/) do
+  visit panels_path
 end
 
 Then(/^縦 (\d+)、横 (\d+) の表が表示される$/) do |height, width|
@@ -60,4 +75,17 @@ end
 
 Then(/^警報パネルの番号が (.*) に変更される$/) do |number|
   expect(Panel.find(@panel.to_param).number).to eq(number)
+end
+
+Then(/^盤 (.*) には警報パネルが (.*)、(.*) の順で表示される$/) do |board_name, panel_one_name, panel_two_name|
+  # 要素が希望の通りになっていることを確認
+  expect(page).to have_selector 'li td', :text => panel_one_name
+  expect(page).to have_selector 'li td', :text => panel_two_name
+
+  # 順番が希望の通りになっていることを確認
+#  expect(page).to have_content(%|#{panel_one_name}.*#{panel_two_name}|)
+end
+
+Then(/^警報パネルの部分は、パネルの詳細のページへのリンクとなっている$/) do
+  expect(page).to have_selector ".panel a"
 end
