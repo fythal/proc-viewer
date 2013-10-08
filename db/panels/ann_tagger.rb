6a29ad0e3e4e0f0fc8512c2da707b96302b0e774
Kazukiyo UEDA <kazukiyo.ueda@gmail.com>
2013-10-09 08:50:42 +0900

    警報のデータのテンプレートを生成するスクリプト

---
+++ db/panels/ann_tagger.rb
@@ -0,0 +1,14 @@
+#!/usr/bin/env ruby
+
+number, height, width = ARGV[0, 3]
+
+data = (1..width.to_i).inject([]) do |result, col|
+  (1..height.to_i).inject(result) do |result, row|
+    result << "  - name: \"\"\n" +
+      "    panel_number: #{number}\n" +
+      "    panel_location: #{row.to_s.tr('1-8', 'a-h')}#{col}\n"
+  end
+end
+
+puts "ann:"
+puts data.join("\n")
