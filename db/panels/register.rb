e0d9f298b76c644dff998f01ddaf74fc1baae4b1
Kazukiyo UEDA <kazukiyo.ueda@gmail.com>
2013-10-11 05:59:09 +0900

    Ann だけでなくオプションにより他のクラスも指定可能にした

--- db/panels/register.rb
+++ db/panels/register.rb
@@ -1,16 +1,50 @@
 #!/usr/bin/env ruby
 # -*- coding: utf-8 -*-
 
+require 'optparse'
 require 'active_resource'
 
-class Ann < ActiveResource::Base
-  self.site = 'http://localhost:3000'
-  self.include_root_in_json = true
+$config = Hash[classname: 'Ann', verbose: false]
+
+opts = OptionParser::new
+opts.on("-c", "--class CLASSNAME") { |classname|
+  $config[:classname] = classname
+}
+opts.on("-v", "--verbose") {
+  $config[:verbose] = true
+}
+opts.on("-h", "--help") {
+  STDERR.puts "Usage: register.rb [-c, --class CLASSNAME] [filename]"
+  STDERR.puts
+  STDERR.puts "   Register objects of class CLASSNAME using data in the given file."
+  STDERR.puts "   If CLASSNAME is omitted, Ann is used as default class."
+  STDERR.puts
+  exit 0
+}
+opts.parse!(ARGV)
+
+
+def myclass
+  @myclass ||= generate_class($config[:classname])
+end
+
+def generate_class(name)
+  klass = Class.new(ActiveResource::Base) do
+    self.site = 'http://localhost:3000'
+    self.include_root_in_json = true
+  end
+
+  puts "Class: #{name.singularize.classify}" if $config[:verbose]
+  Object.const_set(name.singularize.classify, klass)
 end
 
 require 'yaml'
-anns = YAML.load(ARGF)
+items = YAML.load(ARGF)
 
-anns.each do |ann|
-  Ann.create(ann)
+begin
+  items.each do |item|
+    myclass.create(item)
+  end
+rescue Errno::ECONNREFUSED
+  STDERR.puts "Error: Connection refused"
 end
