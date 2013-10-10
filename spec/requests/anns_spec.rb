95e11e9cc4a6c14844b03e024e9d9b978654c743
Kazukiyo UEDA <kazukiyo.ueda@gmail.com>
2013-10-11 05:30:23 +0900

    Ann のレスポンスについてスペックを規定

--- spec/requests/anns_spec.rb
+++ spec/requests/anns_spec.rb
@@ -1,11 +1,38 @@
+# -*- coding: utf-8 -*-
 require 'spec_helper'
 
 describe "Anns" do
-  describe "GET /anns" do
-    it "works! (now write some real specs)" do
-      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
-      get anns_path
-      response.status.should be(200)
+  describe "新しい Ann の作成" do
+    it "新しい Ann を作成して、それの詳細ページへリダイレクトする" do
+      get "/anns/new"
+      expect(response).to render_template(:new)
+
+      post "/anns", :ann => { :name => "My Ann" }
+
+      expect(response).to redirect_to(assigns(:ann))
+      follow_redirect!
+
+      expect(response).to render_template(:show)
+      expect(response.body).to include("Ann was successfully created.")
+    end
+  end
+
+  describe "JSON 形式でデータをゲット" do
+    it "JSON 形式でレスポンス OK" do
+      post anns_url, ann: { name: "My Ann" }, format: "json"
+      expect(response.status).to eq(201)
+    end
+  end
+
+  describe "アップデートをする" do
+    it "属性値を変更する" do
+      post anns_path, :ann => { :name => "My Ann" }, format: "json"
+      get anns_path, format: "json"
+      ann = JSON.parse(response.body).first
+      put ann["url"], :ann => { :name => "Foo" }, format: "json"
+      get anns_path, format: "json"
+      ann = JSON.parse(response.body).first
+      expect(ann["name"]).to eq("Foo")
     end
   end
 end
