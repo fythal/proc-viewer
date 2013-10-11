f7318ee494a93e4cb1c1aaec250f3738f850720a
Kazukiyo UEDA <kazukiyo.ueda@gmail.com>
2013-10-11 11:09:24 +0900

    盤に関するビューとリクエストのスペックを規定

--- spec/views/boards/index.html.erb_spec.rb
+++ spec/views/boards/index.html.erb_spec.rb
@@ -1,20 +1,17 @@
+# -*- coding: utf-8 -*-
 require 'spec_helper'
 
 describe "boards/index" do
   before(:each) do
     assign(:boards, [
-      stub_model(Board,
-        :name => "Name"
-      ),
-      stub_model(Board,
-        :name => "Name"
-      )
-    ])
+             stub_model(Board, :code => "Code", :name => "Name"),
+             stub_model(Board, :code => "Code", :name => "Name"),
+           ])
   end
 
-  it "renders a list of boards" do
+  it "盤の一覧を描画する" do
     render
-    # Run the generator again with the --webrat flag if you want to use webrat matchers
+    assert_select "tr>td", :text => "Code".to_s, :count => 2
     assert_select "tr>td", :text => "Name".to_s, :count => 2
   end
 end
