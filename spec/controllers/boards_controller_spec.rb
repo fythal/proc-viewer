f4963b342684c4ff19b57c86a3e2a761674c3352
Kazukiyo UEDA <kazukiyo.ueda@gmail.com>
2013-10-15 23:51:26 +0900

    Board コントローラの PUT update と DELETE destroy のスペックを規定

--- spec/controllers/boards_controller_spec.rb
+++ spec/controllers/boards_controller_spec.rb
@@ -29,10 +29,43 @@ describe BoardsController do
 
 
   describe "PUT update" do
-    it "updates the requested board" do
+    it "リクエストされた盤の属性をアップデートする" do
       board = Board.create! valid_attributes
       Board.any_instance.should_receive(:update).with({ "code" => "MyNewCode", "name" => "MyNewName" })
       put :update, {:id => board.to_param, :board => { "code" => "MyNewCode", "name" => "MyNewName" }}, valid_session
+    end
+
+    it "リクエストされた盤を @board にアサインする" do
+      board = Board.create! valid_attributes
+      put :update, {:id => board.to_param, :board => valid_attributes}, valid_session
+      assigns(:board).should eq(board)
+    end
+
+    it "リクエストされた盤の属性をアップデートする" do
+      board = Board.create! valid_attributes
+      put :update, {:id => board.to_param, :board => { :code => "foo", :name => "bar" }}, valid_session
+      expect(assigns(:board).code).to eq("foo")
+      expect(assigns(:board).name).to eq("bar")
+    end
+
+    it "リクエストされた盤の詳細画面へリダイレクトする" do
+      board = Board.create! valid_attributes
+      put :update, {:id => board.to_param, :board => valid_attributes}, valid_session
+      response.should redirect_to(board)
+    end
+  end
+
+
+  describe "DELETE destroy" do
+    it "destroys the requested board" do
+      board = Board.create! valid_attributes
+      expect { delete :destroy, {:id => board.to_param}, valid_session }.to change(Board, :count).by(-1)
+    end
+
+    it "redirects to the boards list" do
+      board = Board.create! valid_attributes
+      delete :destroy, {:id => board.to_param}, valid_session
+      response.should redirect_to(boards_url)
     end
   end
 
