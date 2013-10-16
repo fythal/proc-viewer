935ede9be4fe0a98685a8c5024061402c5356fdd
Kazukiyo UEDA <kazukiyo.ueda@gmail.com>
2013-10-16 09:26:12 +0900

    ActiveResource のクラスに Panel と Board を追加

--- db/panels/ar.rb
+++ db/panels/ar.rb
@@ -3,7 +3,26 @@
 
 require 'active_resource'
 
-class Ann < ActiveResource::Base
+class ProcResource < ActiveResource::Base
   self.site = 'http://localhost:3000'
   self.include_root_in_json = true
+
+  def to_param
+    id rescue to_param_using_url
+  end
+end
+
+class Ann < ProcResource
+end
+
+class Board < ProcResource
+end
+
+class Panel < ProcResource
+end
+
+class ActiveResource::Base
+  def to_param_using_url
+    (persisted? and url =~ %r|/anns/(\d+)|) ? $~[1] : nil
+  end
 end
