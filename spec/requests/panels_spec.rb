00bad8cd8bb5d6473158e94c8fdc2fc49697a449
Kazukiyo UEDA <kazukiyo.ueda@gmail.com>
2013-10-16 09:38:09 +0900

    json 形式のレスポンスに number, height, width が含まれることを期待

--- spec/requests/panels_spec.rb
+++ spec/requests/panels_spec.rb
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 require 'spec_helper'
 
 describe "Panels" do
@@ -6,6 +7,18 @@ describe "Panels" do
       # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
       get panels_path
       response.status.should be(200)
+    end
+
+    it "オブジェクトが生成される" do
+      expect { Panel.create!(number: "foo-${Time.now}", height: 3, width: 3) }.to change(Panel, :count).by(1)
+    end
+
+    it "json 形式のレスポンスに期待通りのパラメータが含まれている" do
+      Panel.create!(number: "foo-${Time.now}", height: 3, width: 3)
+
+      get panels_path, format: "json"
+      panel = JSON.parse(response.body).first
+      ["number", "height", "width"].each { |key| expect(panel).to have_key(key) }
     end
   end
 end
