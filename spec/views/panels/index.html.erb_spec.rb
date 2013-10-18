41b804a1af920470c298a4b1fd8d9066161ea2ad
Kazukiyo UEDA <kazukiyo.ueda@gmail.com>
2013-10-19 08:17:40 +0900

    警報パネルの一覧の表示に関する変更
    以下のように表示および内部について変更を行った。
    
      - 盤ごとにグループ分けをしているが、盤の要素 (li) を board クラスとし、
        テキストとして code 属性も表示する。
    
      - 全体を div#list_of_panels で囲む。それによって css の設定が他のビュー
        に影響しないようにする。

--- spec/views/panels/index.html.erb_spec.rb
+++ spec/views/panels/index.html.erb_spec.rb
@@ -4,11 +4,11 @@ require 'spec_helper'
 describe "panels/index" do
   before(:each) do
     assign(:boards, [
-             stub_model(Board, name: "foo board",
+             stub_model(Board, code: "foo-foo", name: "foo board",
                         panels: [
                           stub_model(Panel, :number => "foo-panel-1"),
                           stub_model(Panel, :number => "foo-panel-2")]),
-             stub_model(Board, name: "bar board",
+             stub_model(Board, code: "foo-bar", name: "bar board",
                         panels: [
                           stub_model(Panel, :number => "bar-panel-1"),
                           stub_model(Panel, :number => "bar-panel-2")]),
@@ -17,18 +17,34 @@ describe "panels/index" do
 
   it "盤をリスト形式で描画する" do
     render
-    assert_select "li", :text => /foo board/, :count => 1
-    assert_select "li", :text => /bar board/, :count => 1
+    assert_select "li", :text => /foo-foo  foo board/, :count => 1
+    assert_select "li", :text => /foo-bar  bar board/, :count => 1
   end
 
-  it "盤の下に警報パネルをテーブル形式で描画する" do
+  it "盤のリストを #list_of_panels 要素で囲む" do
+    render
+
+    assert_select "#list_of_panels" do
+      assert_select "li", :text => /foo-foo  foo board/, :count => 1
+      assert_select "li", :text => /foo-bar  bar board/, :count => 1
+    end
+  end
+
+
+  it "盤の HTML 要素を board クラスとする" do
+    render
+    assert_select "li.board", :text => /foo-foo  foo board/, :count => 1
+    assert_select "li.board", :text => /foo-bar  bar board/, :count => 1
+  end
+
+  it "警報パネルを盤の要素の下位に、テーブル形式で描画する" do
     render
     assert_select "li" do
       assert_select "td", :text => "foo-panel-1".to_s, :count => 1
     end
   end
 
-  it "警報パネルのテーブル要素は css の panel クラスである" do
+  it "警報パネルの table 要素を panel クラスとする" do
     render
     assert_select "li" do
       assert_select "td.panel", :text => "foo-panel-1".to_s, :count => 1
