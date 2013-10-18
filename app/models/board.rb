91813cfbc3a9b96cc0133b8d58290aa176b5c8bf
Kazukiyo UEDA <kazukiyo.ueda@gmail.com>
2013-10-19 06:50:07 +0900

    優先的に先に並べる盤を指定可能にし、また盤の code 属性の追加によるソートを実装

--- app/models/board.rb
+++ app/models/board.rb
@@ -1,7 +1,34 @@
+# -*- coding: utf-8 -*-
 class Board < ActiveRecord::Base
+  SORT_PRIORITIZATION = [
+    'H11-P700', 'H11-P701', 'H11-P702',
+    'H11-P704-2', 'H11-P704-1', 'H11-P703',
+    'H11-P705', 'H11-P706',
+  ]
+
   has_many :panels
 
   def <=>(other)
-    name <=> other.name
+    if SORT_PRIORITIZATION.index(code)
+      if SORT_PRIORITIZATION.index(other.code)
+        return SORT_PRIORITIZATION.index(code) <=> SORT_PRIORITIZATION.index(other.code)
+      else
+        return -1
+      end
+    else
+      if SORT_PRIORITIZATION.index(other.code)
+        return 1
+      end
+    end
+
+    result = (code <=> other.code)
+
+    return (code.nil? ? 1 : -1) if result.nil?
+
+    if result == 0
+      return name <=> other.name
+    else
+      return result
+    end
   end
 end
