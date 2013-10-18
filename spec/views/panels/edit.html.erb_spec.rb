990662a4d3fa5822024085408e4f213ebf3fb0dc
Kazukiyo UEDA <kazukiyo.ueda@gmail.com>
2013-10-18 15:59:43 +0900

    警報パネルの新規作成画面と編集画面のスペックを規定し、実装した

--- spec/views/panels/edit.html.erb_spec.rb
+++ spec/views/panels/edit.html.erb_spec.rb
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 require 'spec_helper'
 
 describe "panels/edit" do
@@ -5,16 +6,26 @@ describe "panels/edit" do
     @panel = assign(:panel, stub_model(Panel,
       :number => "MyString"
     ))
+
+    assign(:boards, [
+             stub_model(Board, id: 100, code: "bar-1", name: "Bar 1 ですよ"),
+             stub_model(Board, id: 200, code: "bar-2", name: "Bar 2 ですよ"),
+           ])
   end
 
   it "renders the edit panel form" do
     render
 
-    # Run the generator again with the --webrat flag if you want to use webrat matchers
     assert_select "form[action=?][method=?]", panel_path(@panel), "post" do
       assert_select "input#panel_number[name=?]", "panel[number]"
       assert_select "input#panel_width[name=?]", "panel[width]"
       assert_select "input#panel_height[name=?]", "panel[height]"
+
+      # 盤の選択メニュー
+      assert_select "select", :name => "panel[board_id]" do
+        assert_select "option[value='100']"
+        assert_select "option[value='200']"
+      end
     end
   end
 end
