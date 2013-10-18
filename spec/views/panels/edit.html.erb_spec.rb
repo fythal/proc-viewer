f2820ed14890de601f658a0771f37b3d0d517a46
Kazukiyo UEDA <kazukiyo.ueda@gmail.com>
2013-10-18 15:12:28 +0900

    警報パネルの編集画面にて幅と高さの設定のスペックを規定し、実装した

--- spec/views/panels/edit.html.erb_spec.rb
+++ spec/views/panels/edit.html.erb_spec.rb
@@ -13,6 +13,8 @@ describe "panels/edit" do
     # Run the generator again with the --webrat flag if you want to use webrat matchers
     assert_select "form[action=?][method=?]", panel_path(@panel), "post" do
       assert_select "input#panel_number[name=?]", "panel[number]"
+      assert_select "input#panel_width[name=?]", "panel[width]"
+      assert_select "input#panel_height[name=?]", "panel[height]"
     end
   end
 end
