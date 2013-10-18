a2b73ec36a6558957cc31e23cda4a00210b5d4b5
Kazukiyo UEDA <kazukiyo.ueda@gmail.com>
2013-10-19 06:48:42 +0900

    優先的に先に並べる盤を指定可能とする。また盤の code 属性の追加によるソートのスペックの変更

--- spec/models/board_spec.rb
+++ spec/models/board_spec.rb
@@ -4,12 +4,34 @@ require 'spec_helper'
 describe Board do
   describe "#<=>(other)" do
     before(:each) do
-      @board = Board.new(name: "zzz")
-      @other = Board.new(name: "aaa")
+      @one   = Board.new(code: "A")
+      @two   = Board.new(code: "B")
+      @three = Board.new(name: "AAA")
+      @four  = Board.new(name: "BBB")
+
+      @sp_one    = Board.new(code: "H11-P700")
+      @sp_two    = Board.new(code: "H11-P704-2")
+      @sp_three    = Board.new(code: "H11-P703")
     end
-    it "name を基にソートをする" do
-      expect(@board <=> @other).to eq(1)
-      expect(@other <=> @board).to eq(-1)
+
+    it "code をアルファベット順に比較する" do
+      expect(@one <=> @two).to eq(-1)
+    end
+
+    it "code が設定されていないものより設定されている方が順方向とする" do
+      expect(@two <=> @three).to eq(-1)
+    end
+
+    it "code が設定されていないもの同士では name を比較する" do
+      expect(@three <=> @four).to eq(-1)
+    end
+
+    it "「特別」な盤は一般的なルールが除外され、先に持って来る" do
+      expect(@sp_one <=> @one).to eq(-1)
+    end
+
+    it "「特別」な盤同士でも特定のルールで順番を決める (ルールはアプリケーションの設定)" do
+      expect(@sp_two <=> @sp_three).to eq(-1)
     end
   end
 end
