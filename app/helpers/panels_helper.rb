a128a74dd86f9732558609d56745abf02f46b6e3
Kazukiyo UEDA <kazukiyo.ueda@gmail.com>
2013-10-19 15:17:28 +0900

    縦方向のアルファベットのインデックスをヘルパーで作成

--- app/helpers/panels_helper.rb
+++ app/helpers/panels_helper.rb
@@ -1,2 +1,6 @@
 module PanelsHelper
+  def num_to_alpha(index)
+    index = index.to_i
+    (1..8).include?(index) ? index.to_s.tr('1-8', 'A-H') : ""
+  end
 end
--- spec/helpers/panels_helper_spec.rb
+++ spec/helpers/panels_helper_spec.rb
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 require 'spec_helper'
 
 # Specs in this file have access to a helper object that includes
@@ -11,4 +12,25 @@ require 'spec_helper'
 #   end
 # end
 describe PanelsHelper do
+  describe "num_to_alpha(index)" do
+    describe "index が整数の場合" do
+      it "整数 1-8 をアルファベット A-H に変換する" do
+        expect(num_to_alpha(3)).to eq("C")
+      end
+
+      it "整数 1-8 以外ならば空白を返す" do
+        expect(num_to_alpha(10)).to eq("")
+      end
+    end
+
+    describe "index が文字列の場合" do
+      it "数字 '1'-'8' をアルファベット A-H に変換する" do
+        expect(num_to_alpha("3")).to eq("C")
+      end
+
+      it "数字に変換できなければ空白を返す" do
+        expect(num_to_alpha("x")).to eq("")
+      end
+    end
+  end
 end
